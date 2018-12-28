a2541897c7793825dd4698324c56ed571bf0a5c8
Dina <dcaraballo26@gmail.com>
2018-12-28 16:28:10 -0700

    Refactor lambda functions

--- nordstrom-back-end/handler.js
+++ nordstrom-back-end/handler.js
@@ -4,7 +4,7 @@ const AWS = require('aws-sdk')
 const dynamoDB = new AWS.DynamoDB.DocumentClient(); 
 const TABLE_NAME = 'notes-gql'
  
-module.exports.addNote = (event, context, callback) =>{
+module.exports.createNote = (event, context, callback) => {
   const params = {
     Item : {
       "id" : Date.now(),
@@ -13,14 +13,29 @@ module.exports.addNote = (event, context, callback) =>{
     },
     TableName : process.env.TABLE_NAME
   };
-
-  return dynamoDB.put(params, (error, data) =>{
-    callback(error, data);
+  const response = {
+    statusCode: 200,
+    message: `Item with id: ${params.Item.id} successfully added to database!`
+  }
+  callback(null, response)
+  return dynamoDB.put(params, (error, response) => {
+    callback(error, response);
   });
 }
 
-module.exports.getNotes = () => dynamoDB.scan({ 
-  TableName: process.env.TABLE_NAME 
-}).promise()
-    .then(r => r.Items);
+module.exports.listNotes = async (event, context, callback) => {
+  const result = await dynamoDB.scan({ TableName: process.env.TABLE_NAME }).promise()
+  const notes = result.Items
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: {
+      notes: notes
+    }
+  }
+  callback(null, response)
+};
 
